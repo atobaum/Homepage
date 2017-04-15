ef2997eb7f308186053be4c04d9641d122e6c4e7
Kim Hyunyoung <khyoung2500@gmail.com>
2017-04-16 06:20:58 +0900

    modift gulpfile.js for wiki

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,22 @@ var nodemon = require('gulp-nodemon');
 var browserSync =  require('browser-sync');
 
 var dir = {
+	main: './main',
 	bookshelf: './bookshelf',
-	main: './main'
+	wiki: './wiki'
 };
 
 var src = {
+	main: './main/public',
 	bookshelf: './bookshelf/public',
-	main: './main/public'
+	wiki: dir.wiki + '/public'
 };
 var dist = 'public';
 //...
 gulp.task('browser-sync', function(){
     browserSync.init(null, {
         proxy: "http://localhost:3000",
-        files: ["./public", dir.bookshelf+'/views', dir.main+'/views'],
+        files: ["./public", dir.bookshelf+'/views', dir.main+'/views', dir.wiki+'/views'],
         port: 7000
     });
 });
@@ -27,33 +29,42 @@ gulp.task('start', [], function(){
 	return nodemon({
 			script: './bin/www',
 			ext: 'js',
-			watch: [dir.bookshelf, dir.bookshelf+'/libs', dir.main, dir.main+'/libs']
+			watch: [dir.bookshelf, dir.bookshelf+'/libs', dir.main, dir.main+'/libs', dir.wiki, dir.wiki+'/libs']
 	});
 });
 
 gulp.task('uglify-js', [], function(){
-    gulp.src(src.bookshelf+'/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest(dist+'/js/bookshelf'));
 	gulp.src(src.main+'/js/*.js')
 		.pipe(uglify())
 		.pipe(gulp.dest(dist+'/js'));
+    gulp.src(src.bookshelf+'/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest(dist+'/js/bookshelf'));
+	gulp.src(src.wiki+'/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest(dist+'/js/wiki'));
 });
 
 gulp.task('uglify-css', [], function(){
-	gulp.src(src.bookshelf+'/css/*.css')
-        .pipe(cleanCSS())
-        .pipe(gulp.dest(dist+'/css/bookshelf'));
 	gulp.src(src.main+'/css/*.css')
 		.pipe(cleanCSS())
 		.pipe(gulp.dest(dist+'/css'));
+	gulp.src(src.bookshelf+'/css/*.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(dist+'/css/bookshelf'));
+	gulp.src(src.wiki +'/css/*.css')
+		.pipe(cleanCSS())
+		.pipe(gulp.dest(dist+'/css/wiki'));
+
 });
 
 gulp.task('watch', function(){
-	gulp.watch(src.bookshelf+'/js/*.js', ['uglify-js']);
-	gulp.watch(src.bookshelf+'/css/*.css', ['uglify-css']);
 	gulp.watch(src.main+'/js/*.js', ['uglify-js']);
 	gulp.watch(src.main+'/css/*.css', ['uglify-css']);
+	gulp.watch(src.bookshelf+'/js/*.js', ['uglify-js']);
+	gulp.watch(src.bookshelf+'/css/*.css', ['uglify-css']);
+	gulp.watch(src.wiki+'/js/*.js', ['uglify-js']);
+	gulp.watch(src.wiki+'/css/*.css', ['uglify-css']);
 });
 
 
