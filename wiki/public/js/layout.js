92b6e24165f31fd229658a8b44d1a6025c58ba16
Kim Hyunyoung <khyoung2500@gmail.com>
2017-06-04 01:08:14 +0900

    some tweaks on searchbar: change title to index if page title is empty. namespace unchanges

--- wiki/public/js/layout.js
+++ wiki/public/js/layout.js
@@ -2,11 +2,17 @@
  * Created by Le Reveur on 2017-05-12.
  */
 
+function formatTitle(title){
+    var parsedTitle = /(?:^(.*?):)?(.*?)$/.exec(title);
+    parsedTitle[2] = parsedTitle[2] || "Index";
+    return (parsedTitle[1] ? parsedTitle[1]+":" : "") + parsedTitle[2];
+}
+
 $(function(){
    $('input.prompt')
        .keypress(function(evt){
        if (event.which == 13 || event.keyCode == 13) {
-           window.location.href = "/wiki/view/"+encodeURIComponent($('input.prompt').val());
+           window.location.href = "/wiki/view/"+encodeURIComponent(formatTitle($('input.prompt').val()));
        }
    });
    $('.ui.search').search({
@@ -17,7 +23,7 @@ $(function(){
            results : 'result'
        },
        onSelect: function(result){
-           window.location.href = "/wiki/view/"+encodeURIComponent(result.title);
+           window.location.href = "/wiki/view/"+encodeURIComponent(formatTitle(result.title));
        },
        searchDelay: 500
    });
