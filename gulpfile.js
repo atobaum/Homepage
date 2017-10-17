32859385c3e5edcdbfbe4f959a7d832bd30d6a76
Kim Hyunyoung <khyoung2500@gmail.com>
2017-10-18 05:13:48 +0900

    Add clean to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,37 @@
 "use strict";
 let path = require('path');
 let gulp = require('gulp');
+let gutil = require('gulp-util');
 let uglify = require('gulp-uglify');
 let cleanCSS = require('gulp-clean-css');
+let del = require('del');
 let nodemon = require('gulp-nodemon');
 let ts = require('gulp-typescript');
 let tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('typescript', () => {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest('dist'));
-});
-
-gulp.task('copy-js', () => {
-    return gulp.src(['src/**/*.js', '!src/views/**/*.js'])
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('start', [], function(){
+gulp.task('start', [], function () {
     return nodemon({
         script: './bin/www',
         ext: 'js',
         watch: ['dist']
     });
 });
+gulp.task('clean', () => {
+    return del.sync(['dist', 'public/**/*', '!public/semantic', '!public/semantic/**/*', '!public/favicon.ico']);
+});
 
+//server side
+gulp.task('typescript', () => {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest('dist'));
+});
+gulp.task('copy-js', () => {
+    return gulp.src(['src/**/*.js', '!src/views/**/*.js'])
+        .pipe(gulp.dest('dist'));
+});
+
+//client side
 gulp.task('uglify-js', [], function(){
     gulp.src('src/views/**/*.js')
         .pipe(uglify())
@@ -44,33 +50,43 @@ gulp.task('copy-pug', function () {
 function changeDir(evt, from, to) {
     return path.parse(evt.path).dir.replace(from, to);
 }
+function notify(event) {
+    gutil.log('File', gutil.colors.yellow(event.path), 'was', gutil.colors.magenta(event.type));
+}
 
 gulp.task('watch', function(){
+    //client side
     gulp.watch('src/views/**/*.js', (evt) => {
+        notify(evt);
         return gulp.src(evt.path)
             .pipe(uglify())
             .pipe(gulp.dest(changeDir(evt, 'src' + path.sep + 'views', 'public' + path.sep + 'js')));
     });
     gulp.watch('src/views/**/*.css', (evt) => {
+        notify(evt);
         return gulp.src(evt.path)
             .pipe(cleanCSS())
             .pipe(gulp.dest(changeDir(evt, 'src' + path.sep + 'views', 'public' + path.sep + 'css')));
     });
     gulp.watch('src/views/**/*.pug', (evt) => {
+        notify(evt);
         return gulp.src(evt.path)
             .pipe(gulp.dest(changeDir(evt, 'src' + path.sep + 'views', 'views')));
     });
 
+    //server side
     gulp.watch('src/**/*.ts', evt => {
+        notify(evt);
         return gulp.src(evt.path)
             .pipe(tsProject())
             .js.pipe(gulp.dest(path.parse(evt.path).dir.replace('src', 'dist')));
     });
     gulp.watch(['src/**/*.js', '!src/views/**/*.js'], evt => {
+        notify(evt);
         return gulp.src(evt.path)
             .pipe(gulp.dest(evt.path.replace('src', 'dist')));
     });
-
 });
+gulp.task('compile', ['typescript', 'copy-js', 'uglify-js', 'uglify-css', 'copy-pug']);
 
-gulp.task('default', ['typescript', 'copy-js', 'uglify-js', 'uglify-css', 'copy-pug', 'watch', 'start']);
+gulp.task('default', ['watch', 'start']);
